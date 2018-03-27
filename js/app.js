22fd3cfd52cca05a9b0265bf63b1f0a1bf7f8356
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-03-27 20:14:46 +0200

    delete all the old, divide js code into functions, add all the required functionalty

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 $( document ).ready(function() {
 	/*
-	 * Create a list that holds all of your cards
-	 */
-
-	// get the icons classes
-	let cardsArr = ['fa-diamond', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-paper-plane-o'];
-	// duplicate the icons classes
-	cardsArr = cardsArr.concat(cardsArr);
-	//send the classes to the shuffle function
-	let shuffledArr = shuffle(cardsArr);
-	console.log(shuffledArr);
-
-	/*
 	 * Display the cards on the page
 	 *   - shuffle the list of cards using the provided "shuffle" method below
 	 *   - loop through each card and create its HTML
 	 *   - add each card's HTML to the page
 	 */
+
+	/*
+	 * Create a list that holds all of your cards
+	 */
+	function getCards() {
+		// hide all the stars at first
+		// $(".fa-star").css('display', 'none');
+
+		// get the icons classes
+		let cardsArr = ['fa-diamond', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-paper-plane-o'];
+
+		// duplicate the icons classes
+		cardsArr = cardsArr.concat(cardsArr);
+
+		//send the classes to the shuffle function
+		let shuffledArr = shuffle(cardsArr);
+
+		// draw the DOM
+		drawDom(shuffledArr);
+	}
 
 	// Shuffle function from http://stackoverflow.com/a/2450976
 	function shuffle(array) {
@@ -33,71 +41,153 @@ $( document ).ready(function() {
 	    return array;
 	}
 
-	// get the ul of the cards and empty it
-	let arrOfClicked = [];
 
-	// get the ul of the cards and empty it
-	function reset(){
+	// function draw the DOM
+	function drawDom (array) {
+		$("#deck").empty();
+		var ul = $("#deck");
 
-	  let moves = 0;
-	  $("#moves").text(moves);
+		$.each(array, function(i,className) {
+			var cardStructure = '<li class ="card" cardIndex=' + i + '><i class="fa ' + className + '"></i></li>';
+			// cardStructure.click(clickCard);
+			ul.append(cardStructure);
+		});
+	}
 
-	  let cardsWrapper = $('#deck');
-	  cardsWrapper.empty();
+	//  add click listener to the cards
+	$(".deck").on("click","li",function() {
+		listOfOpenedCards($(this));
+  });
 
-	  for (let i=0; i< shuffledArr.length; i++){
-	    let liStructure = $('<li class="card"></li>');
-	    let iconElement = $('<i class="fa"></i>');
-	    iconElement.addClass(shuffledArr[i]);
+	// array of clicked cards
+	var arrOfOpenedCards = [];
+	function listOfOpenedCards(card){
 
-	    liStructure.append(iconElement);
-	    cardsWrapper.append(liStructure);
+		// check that card doesn't already opend
+		if ( !card.hasClass('open show') ){
+			card.addClass('open show');
 
-	    console.log(arrOfClicked.length);
+			// variables check that doesn't click on the same card
+			var firstIndex = $(arrOfOpenedCards[0]).attr("cardIndex");
+			var secondIndex = $(card).attr("cardIndex");
 
-	    liStructure.click(function() {
+			// check that doesn't click on the same card
+			if ((firstIndex !==  secondIndex)){
 
-	      $(this).addClass("open show");
-	      let iconClasses = $(this).children().attr("class");
-	      arrOfClicked.push(iconClasses);
+				// if the array is empty
+				if(arrOfOpenedCards.length === 0){
+					arrOfOpenedCards.push(card);
+				}else if(arrOfOpenedCards.length === 1){
+					// increase the moves number by 1 every 2 moves
+					movesIcrease();
 
-	      // if there are two clicked cards
-	      if(arrOfClicked.length === 2){
-	        moves = moves + 1;
-	        $("#moves").text(moves);
+					// get the classes of the clicked lis
+					var firstClass = $(arrOfOpenedCards[0]).children().attr("class").toString();
+					var secondClass = $(card).children().attr("class").toString();
+					// compare the classes of the clicked lis
+					if(firstClass === secondClass){
+						match(arrOfOpenedCards[0], card);
+					}else{
+						dontMatch(arrOfOpenedCards[0], card);
+					};
 
-	        // check if they match
-	        if(arrOfClicked[0] === arrOfClicked[1] ){
-	          matchedClass = arrOfClicked[0];
-	          // get their parents to add class matched
-	          $( "i.fa" ).each(function( index ) {
-	            if ($(this).hasClass(matchedClass)){
-	              $(this).parent().addClass("open show match");
-	            }
-	          });
-	        }
-	        // else if they don't match
-	        else{
-	          // get their parent to remove class show
-	          setTimeout(function() {
-	          	$(".card.show.open").removeClass("show open");
-	          }, 500);
-	        }
-	        arrOfClicked = [];
-	      }
-
-	    });
-
-	  }
+				}
+			}
+		}
+	}
 
 
+	// if opened cards matched
+	function match(firstCard, secondCard){
+		firstCard.addClass('match');
+		secondCard.addClass('match');
+		arrOfOpenedCards = [];
+		checkEndOfGame();
+	}
+
+	// if opened card don't match
+	function dontMatch(firstCard, secondCard){
+		setTimeout(function(){
+			firstCard.removeClass('open show');
+			secondCard.removeClass('open show');
+			arrOfOpenedCards = [];
+		}, 400);
+	}
+
+	getCards();
+
+	// restart btn
+	$("#restart").on("click",function() {
+		getCards();
+		clearTimer();
+		timer();
+  });
+
+	// moves number
+	var moves = 0;
+	$("#moves").text(moves);
+	function movesIcrease() {
+		moves = moves + 1;
+		$("#moves").text(moves);
 	};
 
-	reset();
+	// check if all matches, restart the game
+	var matchedTimes = 1;
+	function checkEndOfGame() {
+		if(matchedTimes === 8) {
+			console.log("CONGRATULATIONS You Won *****");
+			rate();
+			setTimeout(function(){
+				getCards ();
+				clearTimer();
+			}, 400);
+		}else{
+			matchedTimes = matchedTimes + 1;
+		}
+	};
 
-	$("#restart").click(function() {
-	  reset();
-	});
+	// timer
+	// used help from (https://jsfiddle.net/Daniel_Hug/pvk6p/)
+  var seconds = 0, minutes = 0, hours = 0, t;
+  function add() {
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
+          minutes = 0;
+          hours++;
+      }
+    }
+
+    $("#timer").text((hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds));
+    timer();
+	}
+	function timer() {
+	  t = setTimeout(add, 1000);
+	}
+	timer();
+	function clearTimer() {
+		$("#timer").text("00:00:00");
+    seconds = 0; minutes = 0; hours = 0;
+	}
+
+	// rating function
+	function rate(){
+		$(".fa-star").each(function(index) {
+		  if(moves <= 14){
+		  	$(this).css('display', 'block');
+			}else if((moves>14) && (moves<=20)){
+				if(index <= 1){
+					$(this).css('display', 'block');
+				}
+			}else{
+				if(index === 0){
+					$(this).css('display', 'block');
+				}
+			}
+		});
+	}
 
 	/*
 	 * set up the event listener for a card. If a card is clicked:
@@ -109,4 +199,6 @@ $( document ).ready(function() {
 	 *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 	 *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 	 */
+
+
 });
